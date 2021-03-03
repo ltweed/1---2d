8a2582bd0b61bf6287d6258593da5af4eed55b60
larrytweed2 <larrytweed@gmail.com>
2021-03-02 23:47:55 -0800

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 6.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if(transform.position.y < -6.5)
+        {
+            float randomX = Random.Range(-11, 11);
+            transform.position = new Vector3(randomX, 6.5f, 0);
+        }
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+            if(player != null)
+            {
+                player.Damage();
+            }
+
+            Destroy(this.gameObject);
+
+        }
+        
+        if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+            
+        }
+
+
+
+        // if other is laser
+
+        // destroy laser
+        // destroy us (in that order)
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemy;
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    private bool _stopSpawning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    // spawn enemies every 5 seconds
+    // create a coroutine 
+    // while loop
+
+    IEnumerator SpawnRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-11f, 11f), 10, 0);
+            //Instantiate enemy prefab
+            GameObject newEnemy = Instantiate(_enemy, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
